cf92be4a087d5d18fe6859f5a70fd74c36c4614b
Sriram-tsr <tangellasriramcharan001@gmail.com>
2024-07-09 13:42:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
+import mongoose from "mongoose";
 
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "PROJECT 0",
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log("Some error occured while connecting to database:", err);
+    });
+};
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
